9a78463c9820a983d39b1c6ab83b4d2734bd99f1
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-24 21:38:12 -0700

    added experience section

--- main.ts
+++ main.ts
@@ -1,20 +1,39 @@
 import './style.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
+  const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
+  let curX = 0;
+  let curY = 0;
+  let tgX = 0;
+  let tgY = 0;
 
-    function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
+  function move() {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(() => {
+      move();
+    });
+  }
+
+  move();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.navbar a');
+
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement: HTMLElement | null = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
         });
-    }
-
-    move();
+      }
+    });
+  });
 });
